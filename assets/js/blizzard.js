583f6f22b17d6e3f115436a8295d5e300e3a0100
Paul Kenny <redpaulkenny@gmail.com>
2019-06-10 03:05:21 +0000

    Fix date string to use timestamp

--- assets/js/blizzard.js
+++ assets/js/blizzard.js
@@ -8,7 +8,7 @@ var token = localStorage.getItem("access_token") || null;
 var expires_at = localStorage.getItem("expires_at") || null;
 
 function getToken() {
-    if (token === null || expires_at < new Date()) {
+    if (token === null || expires_at < Date.now()) {
         return $.ajax({
                 method: "POST",
                 url: "https://eu.battle.net/oauth/token",
@@ -19,20 +19,22 @@ function getToken() {
                     grant_type: "client_credentials"
                 }
             })
-            .done(function(data) {
+            .then(function(data) {
                 localStorage.setItem("access_token", data.access_token);
-                localStorage.setItem("expires_at", new Date() + parseInt(data.expires_in, 10));
+                localStorage.setItem("expires_at", Date.now() + parseInt(data.expires_in, 10));
                 return data.access_token;
             });
     }
     else {
+        console.log("inside else");
         return token;
     }
 }
 
 function getClasses() {
-    return $.when(getToken())
+    return getToken()
         .then(function(token) {
+            console.log(token);
             return $.getJSON("https://eu.api.blizzard.com/wow/data/character/classes", { access_token: token })
                 .then(function(data) {
                     return $(data.classes).map(function() {
@@ -42,6 +44,4 @@ function getClasses() {
         });
 }
 
-$.when(getClasses()).then(function(data) {
-    $("#root").html("this worked");
-});
+getClasses();
