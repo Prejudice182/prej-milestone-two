3ceb4a5cab167b9aa441ba9c8ab05c56a5e3f421
Paul Kenny <redpaulkenny@gmail.com>
2019-06-10 02:26:35 +0000

    Change to jQuery, retrieve classes

--- assets/js/blizzard.js
+++ assets/js/blizzard.js
@@ -1,20 +1,47 @@
+/* global $
+   global localStorage
+*/
 const client_id = "9f6b9ba0b39245708f42e5f93cd1114f";
 const client_secret = "gR6CowcHZDJa5LU348yBI0uhDkGSzTbn";
 
-var request = new XMLHttpRequest();
-var formData = new FormData();
+var token = localStorage.getItem("access_token") || null;
+var expires_at = localStorage.getItem("expires_at") || null;
 
-formData.append("grant_type", "client_credentials");
-
-request.open("POST", "https://eu.battle.net/oauth/token", true);
-request.setRequestHeader("Authorization", "Basic " + window.btoa(client_id + ":" + client_secret));
-
-request.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        var data = JSON.parse(this.response);
-        const access_token = data.access_token;
-        console.log(access_token);
+function getToken() {
+    if (token === null || expires_at < new Date()) {
+        return $.ajax({
+                method: "POST",
+                url: "https://eu.battle.net/oauth/token",
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("Authorization", "Basic " + window.btoa(client_id + ":" + client_secret));
+                },
+                data: {
+                    grant_type: "client_credentials"
+                }
+            })
+            .done(function(data) {
+                localStorage.setItem("access_token", data.access_token);
+                localStorage.setItem("expires_at", new Date() + parseInt(data.expires_in, 10));
+                return data.access_token;
+            });
     }
-};
+    else {
+        return token;
+    }
+}
 
-request.send(formData);
+function getClasses() {
+    return $.when(getToken())
+        .then(function(token) {
+            return $.getJSON("https://eu.api.blizzard.com/wow/data/character/classes", { access_token: token })
+                .then(function(data) {
+                    return $(data.classes).map(function() {
+                        return { id: this.id, name: this.name };
+                    });
+                });
+        });
+}
+
+$.when(getClasses()).then(function(data) {
+    console.log(data);
+});
